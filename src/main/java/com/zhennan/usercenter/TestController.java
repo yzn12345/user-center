14d2eae904a35d4a9eba528368e632f908eb3a57
zhennan <yzhenn5@vt.edu>
2021-09-02 15:16:40 +0800

    Initial commit

---
+++ src/main/java/com/zhennan/usercenter/TestController.java
@@ -0,0 +1,28 @@
+package com.zhennan.usercenter;
+
+
+import com.zhennan.usercenter.dao.user.UserMapper;
+import com.zhennan.usercenter.domain.entity.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+public class TestController {
+
+    @Autowired
+    private UserMapper userMapper;
+    @GetMapping("/test")
+    public User testInsert(){
+        User user = new User();
+        user.setAvatarUrl("xxx");
+        user.setBonus(100);
+        user.setCreateTime(new Date());
+        user.setUpdateTime(new Date());
+        this.userMapper.insertSelective(user);
+        return user;
+
+    }
+}
---
+++ src/main/java/com/zhennan/usercenter/dao/user/UserMapper.java
@@ -0,0 +1,7 @@
+package com.zhennan.usercenter.dao.user;
+
+import com.zhennan.usercenter.domain.entity.user.User;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface UserMapper extends Mapper<User> {
+}
